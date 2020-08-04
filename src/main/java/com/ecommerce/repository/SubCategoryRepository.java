0fb0d2059accd627824082a71983ff3bb97c65fb
Niraj <niraj.kalokhe04@gmail.com>
2020-08-05 00:16:30 +0530

    thamb

--- src/main/java/com/ecommerce/repository/SubCategoryRepository.java
+++ src/main/java/com/ecommerce/repository/SubCategoryRepository.java
@@ -1,6 +1,10 @@
 package com.ecommerce.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.model.SubCategory;
@@ -8,4 +12,9 @@ import com.ecommerce.model.SubCategory;
 @Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, String>{
 
+//	@Query("from SubCategory sub where sub.category =:categoryid")
+//	public List<SubCategory> getSubcategoryByCategory( @Param("category") String categoryid);
+	
+	@Query("FROM  SubCategory t where t.category.id = :category") 
+	List<SubCategory> getSubcategoryByCategory(@Param("category") String category);
 }
